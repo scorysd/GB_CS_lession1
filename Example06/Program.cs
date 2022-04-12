b47f1a5fb33fb7be2069bc3b8b5abb5f84aa3ef2
Roman <toolatet@gmail.com>
2022-04-12 14:45:45 +0700

    Выполнил 6 задачу

---
+++ Example06/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число для проверки: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number%2 == 0)
+{
+    Console.WriteLine("Четное");
+}
+else
+{
+    Console.WriteLine("Нечетное");
+}
