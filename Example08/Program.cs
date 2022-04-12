f94bd4acfa11cd79c6d20c4d8622d1aedc1523f8
Roman <toolatet@gmail.com>
2022-04-12 14:46:08 +0700

    Выполнил 8 задачу

---
+++ Example08/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 0)
+{
+    number = Math.Abs(number);
+}
+for (int i = 2; i < number; i = i + 2)
+{
+    Console.Write($"{i}, ");
+}
+if (number%2 == 0)
+{
+    Console.Write($"{number}");
+}
