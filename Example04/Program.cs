ccb4908de55995b6b760f9de99e5a8bd54d96801
Roman <toolatet@gmail.com>
2022-04-12 14:45:23 +0700

    Выполнил 4 задачу

---
+++ Example04/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.WriteLine("Для сравнения введите 3 числа");
+Console.WriteLine("Первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (b > max)
+ {
+    max = b;    
+ }    
+if (c > max)
+ {
+    max = c;
+ }    
+Console.WriteLine($"Max = {max}");
