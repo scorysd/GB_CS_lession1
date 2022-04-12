e74e5773b02fe41d0e0b0403f6eae130acf079a4
Roman <toolatet@gmail.com>
2022-04-12 14:44:10 +0700

    Выполнил 2 задачу

---
+++ Example02/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.WriteLine("Для сравнения введите 2 числа");
+Console.WriteLine("Первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (b > max)
+ {
+     Console.WriteLine($"Max равен: {b}");
+ }
+else 
+{
+    Console.WriteLine($"Max равен: {a}");
+}
